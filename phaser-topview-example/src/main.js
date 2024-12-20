290bc66255ea03091e5d9f7013bbc2bf2be08f51
joohyuk99 <joohyuk99@gmail.com>
2024-12-20 16:17:00 +0900

    topview example

---
+++ phaser-topview-example/src/main.js
@@ -0,0 +1,35 @@
+import { Boot } from './scenes/Boot';
+import { Game } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
+import { MainMenu } from './scenes/MainMenu';
+import { Preloader } from './scenes/Preloader';
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 768,
+    physics: {
+        default: 'arcade', // 기본 물리 엔진 설정
+        // arcade: {
+        //     gravity: { y: 300 }, // 중력 설정
+        //     debug: false        // 디버그 모드 비활성화
+        // }
+    },
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [
+        Boot,
+        Preloader,
+        MainMenu,
+        Game,
+        GameOver
+    ]
+};
+
+export default new Phaser.Game(config);
